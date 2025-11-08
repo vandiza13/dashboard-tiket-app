cae0b278e0be6952df57736fdae2242326c5f2a8
Mr. Robot <tb.vandiza13@gmail.com>
2025-11-08 21:14:38 +0700

    Fix: riwayat tiket

--- public/script.js
+++ public/script.js
@@ -973,12 +973,12 @@ function formatDateTimeWIB(s) {
   if (isNaN(date.getTime())) return s;
   
   const options = {
+    timeZone: 'Asia/Jakarta',
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
     hour: '2-digit',
     minute: '2-digit',
-    second: '2-digit',
     hour12: false
   };
   
